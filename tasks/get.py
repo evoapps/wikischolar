791dedbc71f4c292f6f66aa3bcbb77462c0fc628
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-04-24 12:50:20 -0500

    Refactored get.py to facilitate testing

--- tasks/get.py
+++ tasks/get.py
@@ -8,10 +8,19 @@ import pypandoc
 @task
 def get():
     """Retrieve a table of articles from a wiki page."""
-    site = pywikibot.Site('en', 'wikipedia')
-    page = pywikibot.Page(site, 'User:Smallbones/1000_random#Data')
-    mediawiki_text = page.text
-    html_text = pypandoc.convert(mediawiki_text, 'html', 'mediawiki')
-    data = pandas.read_html(html_text)
-    data = data[0]  # take the first table only
+    wiki_text = get_page_text('User:Smallbones/1000_random#Data')
+    data = convert_wiki_to_table(wiki_text)
     data.to_csv('1000_random.csv', index=False)
+
+
+def get_page_text(title):
+    site = pywikibot.Site('en', 'wikipedia')
+    page = pywikibot.Page(site, title)
+    return page.text
+
+
+def convert_wiki_to_table(wiki_text):
+    html_text = pypandoc.convert(wiki_text, 'html', 'mediawiki')
+    tables = pandas.read_html(html_text)
+    data = tables[0]  # take the first table only
+    return data
