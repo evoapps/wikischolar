3e20a50e46ca45524c369468ffc91b71e428e15d
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-04-26 09:27:08 -0500

    Write data to directory

--- tasks/get.py
+++ tasks/get.py
@@ -3,14 +3,22 @@ from invoke import task
 import pywikibot
 import pandas
 import pypandoc
+import unipath
+
+DATA = unipath.Path('data')
+if not DATA.exists():
+    DATA.mkdir()
 
 
-@task
-def get():
+@task(help=dict(save_all="Write each stage of the pipeline to the data dir."))
+def get(save_all=False):
     """Retrieve a table of articles from a wiki page."""
     wiki_text = get_page_text('User:Smallbones/1000_random#Data')
     data = convert_wiki_to_table(wiki_text)
-    data.to_csv('1000_random.csv', index=False)
+    data.to_csv(unipath.Path(DATA, 'article_data.csv'), index=False)
+
+    if save_all:
+        unipath.Path(DATA, 'wiki_text.mediawiki').write_file(wiki_text)
 
 
 def get_page_text(title):
