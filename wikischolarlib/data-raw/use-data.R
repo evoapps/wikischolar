cebc756ce427ff67c37b7a31cebf86e6d1479531
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-04-28 22:17:29 -0500

    Basic plots for preliminary report

--- wikischolarlib/data-raw/use-data.R
+++ wikischolarlib/data-raw/use-data.R
@@ -1,6 +1,19 @@
 library(devtools)
+library(dplyr)
+library(lubridate)
 library(readr)
 
 qualities <- read_csv("data-raw/qualities.csv")
+
+# Set order for article classes
+article_classes <- c("Stub", "Start", "C", "B", "GA", "FA")
+qualities$prediction <- factor(qualities$prediction, levels = article_classes)
+
+# Compute article age
+qualities <- qualities %>%
+  group_by(article) %>%
+  mutate(year0 = min(year(timestamp))) %>%
+  ungroup %>%
+  mutate(age = year(timestamp) - year0)
 
 use_data(qualities, overwrite = TRUE)
