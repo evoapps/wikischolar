ec18f2a088eea91d450e07994eddcbf738c0c40c
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-05-02 14:45:52 -0500

    Attempt to compare date and age models

--- qualities.R
+++ qualities.R
@@ -2,9 +2,8 @@
 library(dplyr)
 library(broom)
 library(lubridate)
-
 library(ggplot2)
-
+library(gridExtra)
 library(lme4)
 
 ## ---- data
@@ -27,10 +26,18 @@ max_age <- current_year - wiki_start
 
 scale_x_age <- scale_x_continuous("article age", breaks = seq(0, max_age, by = 2))
 scale_y_quality <- scale_y_continuous("article quality", breaks = 0:6)
-quality_coords <- coord_cartesian(ylim = c(0, 6))
+quality_coords <- coord_cartesian(ylim = c(1, 6))
 
-## ---- quality-by-date
-base_plot +
+## ---- models
+random1000$date <- year(random1000$timestamp) - 2001
+date_mod <- lmer(quality ~ date + (date|title), data = random1000)
+tidy(date_mod, effect = "fixed")
+
+age_mod <- lmer(quality ~ age + (age|title), data = random1000)
+tidy(age_mod, effect = "fixed")
+
+## ---- quality
+date <- base_plot +
   ggtitle("Article quality by date") +
   geom_line(aes(x = timestamp, y = quality, group = title),
             color = colors[["green"]], alpha = 0.2) +
@@ -41,8 +48,7 @@ base_plot +
   scale_y_quality +
   quality_coords
 
-## ---- quality-by-age
-base_plot +
+age <- base_plot +
   ggtitle("Article quality by age") +
   geom_line(aes(x = age, y = quality, group = title),
             color = colors[["blue"]], alpha = 0.2) +
@@ -53,11 +59,13 @@ base_plot +
   scale_y_quality +
   quality_coords
 
+grid.arrange(date, age, nrow = 1)
+
 ## ---- hist-article-ages
 extant <- random1000 %>% group_by(title) %>% filter(age == max(age))
 (base_plot %+% extant) +
   ggtitle("Article ages in sample") +
-  geom_histogram(aes(x = age), binwidth = 1) +
+  geom_histogram(aes(x = age), color = colors[["blue"]], fill = NA, binwidth = 1) +
   scale_x_age
 
 ## ---- age-lmer
