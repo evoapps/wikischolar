3c24064b877ea24e96605956ff4ab2e754607a4c
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-07-26 13:06:34 -0500

    Allow setting DB_NAME using an environment variable

--- wikischolar/db.py
+++ wikischolar/db.py
@@ -1,3 +1,4 @@
+from os import environ
 import sqlite3
 
 import pandas
@@ -6,8 +7,16 @@ import unipath
 DB_NAME = 'wikischolar.sqlite'
 
 def connect(database, must_exist=True):
-    """Return a connection to a sqlite database."""
-    db_loc = unipath.Path(database or DB_NAME)
+    """Return a connection to a sqlite database.
+
+    Args:
+        database (str): Optional. The name of the sqlite database to connect
+            to. Defaults to `wikischolar.sqlite` in the current directory.
+            Can also be set with the environment variable `WIKISCHOLAR_DB`.
+        must_exist (bool): Optional. Defaults to True. Whether or not the
+            connection should fail if the database is not found.
+    """
+    db_loc = unipath.Path(database or environ.get('WIKISCHOLAR_DB') or DB_NAME)
     if must_exist and not db_loc.exists():
         raise MissingDatabaseException(db_loc)
     return sqlite3.connect(db_loc)
