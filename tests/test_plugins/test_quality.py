333bb3fb1537ce968064fe15cc6f866dbe020528
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-08-06 15:16:19 -0500

    Use multiple pytest fixtures

--- tests/test_plugins/test_quality.py
+++ tests/test_plugins/test_quality.py
@@ -3,10 +3,17 @@ import pandas
 
 import wikischolar
 
-@pytest.mark.usefixtures('betamax_session')
+
+@pytest.fixture
+def revisions():
+    return pandas.DataFrame({'revid': [691949610]})
+
+@pytest.mark.usefixtures('betamax_session', 'revisions')
 class TestWP10Quality:
-    def test_wp10_quality(self, betamax_session):
-        revisions = pandas.DataFrame({'revid': [691949610]})
+    def test_wp10_quality(self, betamax_session, revisions):
         wikischolar.plugins.qualities.session = betamax_session
         qualities = wikischolar.plugins.qualities.wp10_qualities(revisions)
         assert len(qualities) == 1
+
+    def test_qualities(self, betamax_session, revisions):
+        pass
