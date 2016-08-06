21bde57116c63fb842ffdc706b321ab0ff6c2d32
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-08-06 15:14:27 -0500

    Rolled quality plugin tests into a class

--- tests/test_plugins/test_quality.py
+++ tests/test_plugins/test_quality.py
@@ -3,10 +3,10 @@ import pandas
 
 import wikischolar
 
-
 @pytest.mark.usefixtures('betamax_session')
-def test_wp10_quality(betamax_session):
-    revisions = pandas.DataFrame({'revid': [691949610]})
-    wikischolar.plugins.qualities.session = betamax_session
-    qualities = wikischolar.plugins.qualities.wp10_qualities(revisions)
-    assert len(qualities) == 1
+class TestWP10Quality:
+    def test_wp10_quality(self, betamax_session):
+        revisions = pandas.DataFrame({'revid': [691949610]})
+        wikischolar.plugins.qualities.session = betamax_session
+        qualities = wikischolar.plugins.qualities.wp10_qualities(revisions)
+        assert len(qualities) == 1
